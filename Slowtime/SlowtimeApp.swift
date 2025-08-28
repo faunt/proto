b1238e0f946c71936a7d115584fbf9a5f2dfc682
faunt <faunt@users.noreply.github.com>
2025-08-28 11:28:25 +0100

    Initial Commit

---
+++ Slowtime/SlowtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlowtimeApp.swift
+//  Slowtime
+//
+//  Created by Dave Morgan on 28/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SlowtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
